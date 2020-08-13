73a470423b94894f14372d632f1192acf25a42e4
Pavan <pavanpabs@gmail.com>
2020-08-13 15:16:04 +0530

    Validations aded

--- ScheduleWindow.xaml.cs
+++ ScheduleWindow.xaml.cs
@@ -32,6 +32,8 @@ namespace TimeTableManager
         {
             this.dbContext1 = dbContext;
             InitializeComponent();
+            this.duration.SelectedIndex = 0;
+            ResetButton.IsEnabled = false;
             GetSchedule();
 
             AddNewScheduleGrid.DataContext = NewSchedule;
@@ -45,14 +47,18 @@ namespace TimeTableManager
 
         private void AddSchedule(Object s, RoutedEventArgs e)
         {
+            Boolean validator=true;
+     
             //Checkbox manipulation
             var checkString = "";
             if (allChecked.Count == 0)
             {
-                MessageBox.Show("No Strings Checked");
+                validator = false;
+                MessageBox.Show("No working days Checked");
             }
             else
             {
+               
                 for (int i = 0; i < allChecked.Count; i++)
                 {
                     if (i == 0)
@@ -66,25 +72,53 @@ namespace TimeTableManager
                 }
 
             }
-            //insert checkbox values to object
-           NewSchedule.Working_days = checkString;
-           NewSchedule.Working_duration = duration.Text;
 
-            //insert that object to database
-            dbContext1.Schedules.Add(NewSchedule);
-            dbContext1.SaveChanges();
+            //validations
+            if (allChecked.Count != NewSchedule.Working_days_count) {
+                validator = false;
+                MessageBox.Show("Please check the same number of days that you have entered in Working days per week count!");
+                
+            }
+            if (NewSchedule.working_time_hrs>14 || NewSchedule.working_time_hrs<1)
+            {
+                validator = false;
+                MessageBox.Show("Working hours out of range");
+            }
+            if (NewSchedule.Working_time_mins >= 60 || NewSchedule.Working_time_mins< 0)
+            {
+                validator = false;
+                MessageBox.Show("Working minutes out of range");
+                
+            }
+            if (NewSchedule.Working_duration==null)
+            {
+                validator = false;
+                MessageBox.Show("Please select a timeslot duration!");
 
-            MondayCheckBox.IsChecked = false;
-            TuesdayCheckBox.IsChecked = false;
-            WednessdayCheckBox.IsChecked = false;
-            ThursdayCheckBox.IsChecked = false;
-            FridayCheckBox.IsChecked = false;
-            SaturdayCheckBox.IsChecked = false;
-            SundayCheckBox.IsChecked = false;
+            }
+            if (validator) {
+                //insert checkbox values to object
+                NewSchedule.Working_days = checkString;
+                NewSchedule.Working_duration = duration.Text;
 
-            GetSchedule();
-            NewSchedule = new Schedule();
-            AddNewScheduleGrid.DataContext = NewSchedule;
+                //insert that object to database
+                dbContext1.Schedules.Add(NewSchedule);
+                dbContext1.SaveChanges();
+
+                MondayCheckBox.IsChecked = false;
+                TuesdayCheckBox.IsChecked = false;
+                WednessdayCheckBox.IsChecked = false;
+                ThursdayCheckBox.IsChecked = false;
+                FridayCheckBox.IsChecked = false;
+                SaturdayCheckBox.IsChecked = false;
+                SundayCheckBox.IsChecked = false;
+
+                GetSchedule();
+                NewSchedule = new Schedule();
+                AddNewScheduleGrid.DataContext = NewSchedule;
+
+            }
+            
         }
         private void myCheckBox_Checked(object sender, RoutedEventArgs e)
         {
@@ -100,6 +134,8 @@ namespace TimeTableManager
 
         private void UpdateScheduleForEdit(Object s, RoutedEventArgs e)
         {
+            ResetButton.IsEnabled = true;
+
             AddButton.IsEnabled = false;
             MondayCheckBox.IsChecked = false;
             TuesdayCheckBox.IsChecked = false;
@@ -155,10 +191,12 @@ namespace TimeTableManager
 
         private void UpdateSchedule(Object s, RoutedEventArgs e)
         {
+            Boolean validator = true;
             //Checkbox manipulation
             var checkString = "";
             if (allChecked.Count == 0)
             {
+                validator = false;
                 MessageBox.Show("No Strings Checked");
             }
             else
@@ -177,46 +215,100 @@ namespace TimeTableManager
 
             }
 
-            SelectedSchedule.Working_days = checkString;
-            SelectedSchedule.Working_duration = duration.Text;
+            //validations
+            if (allChecked.Count != SelectedSchedule.Working_days_count)
+            {
+                validator = false;
+                MessageBox.Show("Please check the same number of days that you have entered in Working days per week count!");
 
-            dbContext1.Update(SelectedSchedule);
-            dbContext1.SaveChanges();
-            MondayCheckBox.IsChecked = false;
-            TuesdayCheckBox.IsChecked = false;
-            WednessdayCheckBox.IsChecked = false;
-            ThursdayCheckBox.IsChecked = false;
-            FridayCheckBox.IsChecked = false;
-            SaturdayCheckBox.IsChecked = false;
-            SundayCheckBox.IsChecked = false;
+            }
+            if (SelectedSchedule.working_time_hrs > 14 || SelectedSchedule.working_time_hrs < 1)
+            {
+                validator = false;
+                MessageBox.Show("Working hours out of range");
+            }
+            if (SelectedSchedule.Working_time_mins >= 60 || SelectedSchedule.Working_time_mins < 0)
+            {
+                validator = false;
+                MessageBox.Show("Working minutes out of range");
 
-            GetSchedule();
-            NewSchedule = new Schedule();
-            AddNewScheduleGrid.DataContext = NewSchedule;
-            AddButton.IsEnabled = true;
+            }
+            if (SelectedSchedule.Working_duration == null)
+            {
+                validator = false;
+                MessageBox.Show("Please select a timeslot duration!");
+
+            }
+
+            if (validator) 
+            {
+                SelectedSchedule.Working_days = checkString;
+                SelectedSchedule.Working_duration = duration.Text;
+
+                dbContext1.Update(SelectedSchedule);
+                dbContext1.SaveChanges();
+                MondayCheckBox.IsChecked = false;
+                TuesdayCheckBox.IsChecked = false;
+                WednessdayCheckBox.IsChecked = false;
+                ThursdayCheckBox.IsChecked = false;
+                FridayCheckBox.IsChecked = false;
+                SaturdayCheckBox.IsChecked = false;
+                SundayCheckBox.IsChecked = false;
+
+                GetSchedule();
+                NewSchedule = new Schedule();
+                AddNewScheduleGrid.DataContext = NewSchedule;
+                AddButton.IsEnabled = false;
+
+            }
+
+            
         }
 
         private void DeleteSchedule(Object s, RoutedEventArgs e)
         {
-            SelectedSchedule = (s as FrameworkElement).DataContext as Schedule;
-            dbContext1.Remove(SelectedSchedule);
-            dbContext1.SaveChanges();
+            MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Are you sure?", "Delete Confirmation", System.Windows.MessageBoxButton.YesNo);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                SelectedSchedule = (s as FrameworkElement).DataContext as Schedule;
+                dbContext1.Remove(SelectedSchedule);
+                dbContext1.SaveChanges();
 
-            MondayCheckBox.IsChecked = false;
-            TuesdayCheckBox.IsChecked = false;
-            WednessdayCheckBox.IsChecked = false;
-            ThursdayCheckBox.IsChecked = false;
-            FridayCheckBox.IsChecked = false; 
-            SaturdayCheckBox.IsChecked = false; 
-            SundayCheckBox.IsChecked = false;
+                MondayCheckBox.IsChecked = false;
+                TuesdayCheckBox.IsChecked = false;
+                WednessdayCheckBox.IsChecked = false;
+                ThursdayCheckBox.IsChecked = false;
+                FridayCheckBox.IsChecked = false;
+                SaturdayCheckBox.IsChecked = false;
+                SundayCheckBox.IsChecked = false;
 
-            GetSchedule();
-            NewSchedule = new Schedule();
-            AddNewScheduleGrid.DataContext = NewSchedule;
-            AddButton.IsEnabled = true;
-
+                GetSchedule();
+                NewSchedule = new Schedule();
+                AddNewScheduleGrid.DataContext = NewSchedule;
+                AddButton.IsEnabled = true;
+                ResetButton.IsEnabled = false;
+            }
         }
 
-       
+        private void ResetSchedule(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Are you sure?", "Reset Confirmation", System.Windows.MessageBoxButton.YesNo);
+            if (messageBoxResult == MessageBoxResult.Yes) {
+                NewSchedule = new Schedule();
+                AddNewScheduleGrid.DataContext = NewSchedule;
+                MondayCheckBox.IsChecked = false;
+                TuesdayCheckBox.IsChecked = false;
+                WednessdayCheckBox.IsChecked = false;
+                ThursdayCheckBox.IsChecked = false;
+                FridayCheckBox.IsChecked = false;
+                SaturdayCheckBox.IsChecked = false;
+                SundayCheckBox.IsChecked = false;
+
+                AddButton.IsEnabled = true;
+                ResetButton.IsEnabled = false;
+            }
+
+            
+        }
     }
 }
